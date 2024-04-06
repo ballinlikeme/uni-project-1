5662c876207e30ab19521e6123cce2a07dc89d90
Maxim Spirkin <maksim.spirkin@sunmait.tech>
2024-04-06 18:09:36 +0300

    fixed deps

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { SNMTUiContextProvider } from "@snmt-react-ui/core";
 import "@fontsource-variable/inter";
 import "@fontsource-variable/jetbrains-mono";
 import "./index.css";
@@ -12,10 +11,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <SNMTUiContextProvider value={{ locale: "ru", theme: "dark" }}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </SNMTUiContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
